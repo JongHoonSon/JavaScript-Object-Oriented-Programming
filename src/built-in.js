4392eb0206eeaae92d962900fa166f325060cbcc
JongHoonSon <roka1684@gmail.com>
2022-05-03 21:12:34 +0900

    4.1. 객체는 언제 쓰나요?

---
+++ src/built-in.js
@@ -0,0 +1,6 @@
+// 우리가 기존에 사용했던 Math는 JS에서 기본적으로 제공하는 객체이다.
+// PI는 객체의 일반 속성, random과 floor는 객체의 함수형태의 속성이다.
+
+console.log("Math.PI", Math.PI);
+console.log("Math.random()", Math.random());
+console.log("Math.floor(3.9)", Math.floor(3.9));
