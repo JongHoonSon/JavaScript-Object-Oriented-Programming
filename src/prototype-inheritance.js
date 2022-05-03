74714b7934028987e2bf95eee4cfa6593e46a899
JongHoonSon <roka1684@gmail.com>
2022-05-03 23:25:32 +0900

    13.3. Object.create()
    subObj는 superObj를 Object.create로 생성한 객체가 됨(superObj의 속성을 모두 물려받음)

--- src/prototype-inheritance.js
+++ src/prototype-inheritance.js
@@ -2,6 +2,8 @@ var superObj = { superVal: "super" };
 // var subObj = { subVal: "sub" };
 
 // subObj.__proto__ = superObj;
+var subObj = Object.create(superObj);
+subObj.subVal = "sub";
 console.log("subObj.subVal =>", subObj.subVal);
 console.log("subObj.superVal =>", subObj.superVal);
 
