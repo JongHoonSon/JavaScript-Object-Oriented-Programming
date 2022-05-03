67daebcb5ffa06acc7b6ec704a2d1686602649a6
JongHoonSon <roka1684@gmail.com>
2022-05-03 23:45:39 +0900

    14.2. call
    sum 함수가 매개변수를 사용한다면,
    sum의 인자에는 this의 정보가 첫번째로 오고,
    그 다음 sum 함수에서 사용되는 매개변수가 옴

--- src/object-function.js
+++ src/object-function.js
@@ -1,12 +1,12 @@
 var kim = { name: "kim", first: 10, second: 20 };
 var lee = { name: "lee", first: 10, second: 10 };
 
-function sum() {
-  return this.first + this.second;
+function sum(prefix) {
+  return prefix + (this.first + this.second);
 }
 
 sum.call(kim);
 
 // sum이 kim과 lee의 멤버가 됨
-console.log("sum.call(kim)", sum.call(kim));
-console.log("sum.call(lee)", sum.call(lee));
+console.log("sum.call(kim)", sum.call(kim, " => "));
+console.log("sum.call(lee)", sum.call(lee, " : "));
