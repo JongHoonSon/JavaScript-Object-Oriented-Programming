186c9701fc40d818ab2dcfc83e17113ec6ceb63f
JongHoonSon <roka1684@gmail.com>
2022-05-03 22:39:25 +0900

    10. 메소드 구현
    프로토타입으로 정의된 sum 메소드 실행

--- src/class.js
+++ src/class.js
@@ -22,5 +22,5 @@ console.log("lee", lee);
 //   return "this : " + (this.first + this.second + this.third);
 // };
 // var park = new Person("park", 10, 10, 10);
-// console.log("lee.sum()", lee.sum());
+console.log("lee.sum()", lee.sum());
 // console.log("park.sum()", park.sum());
