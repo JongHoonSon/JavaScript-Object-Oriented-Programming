8bf5f7b48132df8b2f7f9d314624cc9526ea9294
JongHoonSon <roka1684@gmail.com>
2022-05-03 23:13:04 +0900

    13.2. __proto__
    prototype link를 이용해 subObj가 superObj를 상속하도록 함

---
+++ src/prototype-inheritance.js
@@ -0,0 +1,6 @@
+var superObj = { superVal: "super" };
+var subObj = { subVal: "sub" };
+
+subObj.__proto__ = superObj;
+console.log("subObj.subVal =>", subObj.subVal);
+console.log("subObj.superVal =>", subObj.superVal);
