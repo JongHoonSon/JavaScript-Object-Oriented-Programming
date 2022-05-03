76266ef40e97a703a2fadef7ef6a8120f944aa2f
JongHoonSon <roka1684@gmail.com>
2022-05-03 22:32:59 +0900

    8.2. Classes 의 생성

---
+++ src/class.js
@@ -0,0 +1,11 @@
+class Person {}
+
+var lee = new Person();
+console.log("lee", lee);
+// var lee = new Person("lee", 10, 20, 30);
+// lee.sum = function () {
+//   return "this : " + (this.first + this.second + this.third);
+// };
+// var park = new Person("park", 10, 10, 10);
+// console.log("lee.sum()", lee.sum());
+// console.log("park.sum()", park.sum());
