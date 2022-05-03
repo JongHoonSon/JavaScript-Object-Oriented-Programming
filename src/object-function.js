e6e5aa7ca9cd25c4ee63e2d9170762b17a8613a2
JongHoonSon <roka1684@gmail.com>
2022-05-03 23:48:34 +0900

    14.3. bind
    bind를 통해 특정 함수에서 쓰이는 this의 객체 정보를 고정시킨
    새로운 함수를 만들 수 있음
    (기존의 함수에는 영향x)

--- src/object-function.js
+++ src/object-function.js
@@ -10,3 +10,5 @@ sum.call(kim);
 // sum이 kim과 lee의 멤버가 됨
 console.log("sum.call(kim)", sum.call(kim, " => "));
 console.log("sum.call(lee)", sum.call(lee, " : "));
+var kimSum = sum.bind(kim, " -> ");
+console.log("kimSum()", kimSum());
