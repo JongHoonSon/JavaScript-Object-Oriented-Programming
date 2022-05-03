9185ecb74439ed71dada4671127ec7970e6a673a
JongHoonSon <roka1684@gmail.com>
2022-05-03 22:36:29 +0900

    9. class의 constructor function

--- src/class.js
+++ src/class.js
@@ -1,11 +1,16 @@
 class Person {
-  // Person 타입의 새로운 객체가 생성될 때, cunstructor가 실행됨
-  constructor() {
+  // Person 타입의 새로운 객체가 생성될 때, constructor가 실행됨
+  // constructor에서 객체의 기본 속성의 값 정의
+  constructor(name, first, second, third) {
+    this.name = name;
+    this.first = first;
+    this.second = second;
+    this.third = third;
     console.log("constructor");
   }
 }
 
-var lee = new Person();
+var lee = new Person("lee", 10, 20, 30);
 console.log("lee", lee);
 // var lee = new Person("lee", 10, 20, 30);
 // lee.sum = function () {
