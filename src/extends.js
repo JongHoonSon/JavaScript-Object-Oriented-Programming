32f037dfc36a137b609ce8eb3a7abaf81d074635
JongHoonSon <roka1684@gmail.com>
2022-05-03 22:47:05 +0900

    11. 상속
    Person을 extends한 PersonPlus를 타입을 만들면
    PersonPlus는 Person의 속성을 그대로 가져와 사용할 수 있다.

---
+++ src/extends.js
@@ -0,0 +1,22 @@
+class Person {
+  constructor(name, first, second, third) {
+    this.name = name;
+    this.first = first;
+    this.second = second;
+    this.third = third;
+    console.log("constructor");
+  }
+  sum() {
+    return "prototype : " + (this.first + this.second + this.third);
+  }
+}
+
+class PersonPlus extends Person {
+  avg() {
+    return (this.first + this.second) / 2;
+  }
+}
+
+var lee = new PersonPlus("lee", 10, 20, 30);
+console.log("lee.sum()", lee.sum());
+console.log("lee.avg()", lee.avg());
