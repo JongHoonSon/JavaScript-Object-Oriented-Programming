92814ea30d67e92ce14eb99b260ebaf4b7fcbe68
JongHoonSon <roka1684@gmail.com>
2022-05-03 21:04:10 +0900

    3.2. 객체와 반복문
    반복문으로 배열의 모든 원소에 접근

--- src/object_loop.js
+++ src/object_loop.js
@@ -1,5 +1,10 @@
 // 배열 생성
 var memberArray = ["egoing", "graphittie", "leezhce"];
+var i = 0;
+while (i < memberArray.length) {
+  console.log(i, memberArray[i]);
+  i = i + 1;
+}
 
 // 객체 생성
 var memberObject = {
